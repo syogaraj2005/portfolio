4a55dd0eb0ab239ac73a5686ec5b7f197dab1b6a
YOGARAJ S <syogaraj2005@gmail.com>
2026-09-08 11:36:06 +0530

    Fix base path for GitHub Pages and Netlify

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/portfolio/', // <-- Small letters '/portfolio/' aaga irukka vendum
+  base: './', // <-- './' veithaal GitHub Pages matrum Netlify irandilume assets correct-aa load aagum
 })
